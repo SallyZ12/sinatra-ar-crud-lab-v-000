c779c8853db8a0e30ebdea0c08d4c1694f03a480
Sally Zasloff <szasloff@gmail.com>
2018-02-16 16:09:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do #finds a single post
-    binding.pry
+    # binding.pry
       @post = Post.find_by(params[:id])
       erb :show
   end
